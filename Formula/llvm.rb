33e41e4dfabfd8676b74dca673ca47dca1685ceb
miccal <miccal.matthews@gmail.com>
2021-07-01 07:27:41 +0800

    llvm: revision bump (libffi 3.4.2)

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -5,7 +5,7 @@ class Llvm < Formula
   sha256 "9ed1688943a4402d7c904cc4515798cdb20080066efa010fe7e1f2551b423628"
   # The LLVM Project is under the Apache License v2.0 with LLVM Exceptions
   license "Apache-2.0" => { with: "LLVM-exception" }
-  revision 1
+  revision 2
   head "https://github.com/llvm/llvm-project.git", branch: "main"
 
   livecheck do
